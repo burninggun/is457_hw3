946c2e101fdac8d69c8567e0af68b1d8b94a4d37
burninggun <willchengsters@gmail.com>
2020-03-12 18:41:09 -0700

    completed assignment

--- src/app/service/quotes.service.ts
+++ src/app/service/quotes.service.ts
@@ -11,4 +11,14 @@ export class QuotesService {
     this.favoriteQuotes.push(quote);
     console.log(this.favoriteQuotes);
   }
+  getFavoriteQuotes = () => {
+    return this.favoriteQuotes
+  }
+  removeQuoteFromFavorite = (quote: Quotes) => {
+    for(let i = 0; i<this.favoriteQuotes.length; i++){
+      if(this.favoriteQuotes[i].id === quote.id ){
+        this.favoriteQuotes.splice(i,1);
+      }
+    }
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuotesService } from '../service/quotes.service';
+import { Quotes } from 'src/assets/data/quotes.interface';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  quotes: Quotes[];
+  constructor( private quoteservice: QuotesService) {}
+  ionViewWillEnter() {
+    this.quotes = this.quoteservice.getFavoriteQuotes();
+    console.log(this.quotes)
+  }
+  onRemoveQuoteFromFavorite(quote: Quotes){
+    this.quoteservice.removeQuoteFromFavorite(quote)
+  }
 }
